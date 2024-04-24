4db8fec25557b8a6685dc5f2b1bfd16df229bf14
MariTTH <mariia@Mariia>
2024-04-24 20:48:25 +0300

    add

---
+++ src/main/java/com/example/JSFLab/PointData.java
@@ -0,0 +1,116 @@
+package com.example.JSFLab;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@ManagedBean(name="pointData")
+@SessionScoped
+public class PointData {
+    private Double x;
+    private Double y;
+    private Double r;
+    private String date;
+    private String duration;
+    private String match;
+
+    private boolean clickedOnGraph;
+
+    public boolean isClickedOnGraph() {
+        return clickedOnGraph;
+    }
+
+    public void setClickedOnGraph(boolean clickedOnGraph) {
+        this.clickedOnGraph = clickedOnGraph;
+    }
+
+    public Double getX() {
+        return x;
+    }
+
+    public void setX(Double x) {
+        this.x = x;
+    }
+
+    public Double getY() {
+        return y;
+    }
+
+    public void setY(Double y) {
+        this.y = y;
+    }
+
+    public Double getR() {
+        return r;
+    }
+
+    public void setR(Double r) {
+        this.r = r;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getMatch() {
+        return match;
+    }
+
+    public void setMatch(String match) {
+        this.match = match;
+    }
+
+    public PointData calculateHit() {
+        long start = System.nanoTime();
+
+        if (x > 0) {
+            if (y >= 0 && y <= r/2 && x <= r)
+                match = "Да";
+            else
+                match = "Нет";
+        } else {
+            if (y >= 0 && y <= x + r && x<=0)
+                match = "Да";
+            else if (y <= 0 && x*x + y*y <= r*r)
+                match = "Да";
+            else
+                match = "Нет";
+        }
+
+        date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"));
+        duration = (System.nanoTime() - start) / 1000 + "мкс";
+        return this;
+    }
+
+    public boolean check() {
+
+        if (x > 0) {
+            if (y >= 0 && y <= r/2 && x <= r)
+                return true;
+            else
+                return false;
+        } else {
+            if (y >= 0 && y <= x + 4)
+                return true;
+            else if (y <= 0 && x*x + y*y <= r*r)
+                return true;
+            else
+                return false;
+        }
+
+
+    }
+}
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,2 +1,118 @@
-package PACKAGE_NAME;public class Test {
+import com.example.JSFLab.PointData;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class Test {
+    private final PointData result = new PointData();
+
+    @org.junit.Test
+    public void hitCircle() {
+        result.setX(-2.0);
+        result.setY(-2.0);
+        result.setR(1.0);
+        assertFalse(result.check());
+    }
+
+    @org.junit.Test
+    public void hitCircleFarBorder() {
+        result.setX(-1.6);
+        result.setY(-1.15);
+        result.setR(2.0);
+        assertTrue(result.check());
+    }
+
+    @org.junit.Test
+    public void hitCircleLowBorder() {
+        result.setX(-0.8);
+        result.setY(0.05);
+        result.setR(2.0);
+        assertTrue(result.check());
+    }
+
+
+    @org.junit.Test
+    public void missCircle() {
+        result.setX(-3.5);
+        result.setY(-1.6);
+        result.setR(2.0);
+        assertFalse(result.check());
+    }
+
+    @org.junit.Test
+    public void missCircleFarBorder() {
+        result.setX(-0.8);
+        result.setY(-1.9);
+        result.setR(2.0);
+        assertFalse(result.check());
+    }
+
+    @org.junit.Test
+    public void missCircleLowBorder() {
+        result.setX(-2.0);
+        result.setY(-0.49);
+        result.setR(2.0);
+        assertFalse(result.check());
+    }
+
+    @org.junit.Test
+    public void hitTriangle() {
+        result.setX(-1.0);
+        result.setY(1.25);
+        result.setR(3.0);
+        assertTrue(result.check());
+    }
+
+    @org.junit.Test
+    public void hitTriangleLowBorder() {
+        result.setX(-2.5);
+        result.setY(-0.75);
+        result.setR(3.0);
+        assertTrue(result.check());
+    }
+
+    @org.junit.Test
+    public void hitTriangleTopBorder() {
+        result.setX(-0.9);
+        result.setY(2.41);
+        result.setR(3.0);
+        assertTrue(result.check());
+    }
+
+    @org.junit.Test
+    public void missTriangle() {
+        result.setX(-3.4);
+        result.setY(3.35);
+        result.setR(3.0);
+        assertFalse(result.check());
+    }
+    
+
+    @org.junit.Test
+    public void hitRectangle() {
+        result.setX(1.7);
+        result.setY(0.78);
+        result.setR(3.0);
+        assertTrue(result.check());
+    }
+
+    @org.junit.Test
+    public void missRectangle() {
+        result.setX(3.8);
+        result.setY(3.05);
+        result.setR(3.0);
+        assertFalse(result.check());
+    }
+
+    @org.junit.Test
+    public void rSwitch() {
+        result.setX(-0.5);
+        result.setY(0.0);
+        result.setR(1.0);
+        assertTrue(result.check());
+        result.setR(2.5);
+        assertTrue(result.check());
+        result.setR(3.0);
+        assertTrue(result.check());
+    }
 }
